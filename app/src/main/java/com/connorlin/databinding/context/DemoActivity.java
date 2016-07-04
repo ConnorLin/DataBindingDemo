23fe8994e2a6221b2ce1aee18411b11872c163bd
ConnorLin <lgr322145@163.com>
2016-07-04 10:03:59 +0800

    finish demo

--- app/src/main/java/com/connorlin/databinding/context/DemoActivity.java
+++ app/src/main/java/com/connorlin/databinding/context/DemoActivity.java
@@ -2,8 +2,6 @@ package com.connorlin.databinding.context;
 
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
-import android.widget.ListView;
-import android.widget.TextView;
 
 import com.connorlin.databinding.DemoBinding;
 import com.connorlin.databinding.R;
@@ -13,15 +11,11 @@ import com.connorlin.databinding.model.ObservableContact;
 
 public class DemoActivity extends BaseActivity {
 
-    private TextView mEmpty;
-    private ListView mListView;
-
     private DemoBinding mDemoBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_demo);
 
         mDemoBinding = DataBindingUtil.setContentView(this, R.layout.activity_demo);
         ObservableContact contact = new ObservableContact("", "");
@@ -31,12 +25,10 @@ public class DemoActivity extends BaseActivity {
     }
 
     private void initViews() {
-        mEmpty = (TextView) findViewById(R.id.empty_view);
-        mListView = (ListView) findViewById(R.id.listView);
-        mListView.setEmptyView(mEmpty);
+        mDemoBinding.listView.setEmptyView(mDemoBinding.emptyView);
 
         ListAdapter listAdapter = new ListAdapter(this);
-        mListView.setAdapter(listAdapter);
+        mDemoBinding.listView.setAdapter(listAdapter);
 
         DemoHandler viewHandler = new DemoHandler(this, mDemoBinding, listAdapter);
         mDemoBinding.setHandler(viewHandler);
